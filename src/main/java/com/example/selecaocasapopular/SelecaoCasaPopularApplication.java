25b16c43759c5b93cc55746b5eb227236864d546
PCROBSON\robson.ribeiro <robson.ribeiro@sefaz.pb.gov.br>
2024-08-01 02:01:43 -0300

    Criando Projeto

---
+++ src/main/java/com/example/selecaocasapopular/SelecaoCasaPopularApplication.java
@@ -0,0 +1,13 @@
+package com.example.selecaocasapopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoCasaPopularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelecaoCasaPopularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selecaocasapopular/SelecaoCasaPopularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selecaocasapopular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoCasaPopularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
